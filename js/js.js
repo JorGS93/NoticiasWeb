0ee7e4fffdc8f4120c22248e5f3a378be3d0c060
JorGS93 <jordigenovartsanchez1993@gmail.com>
2017-05-26 18:11:43 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -9,8 +9,11 @@ $('#mainNav').affix({
 	
 
 
-$(document).ready(function()
-{
+$(document).ready(function(){
+	$.getJSON( "https://rawgit.com/JorGS93/NoticiasWeb/master/js/news.json", function( jsonObject ) {
+     ponerTitulo( jsonObject );
+	});
+
 
 $('#boton').click(function(event) {
 $('#cajon2').show(500);
@@ -21,11 +24,6 @@ $('#boton').hide();
 
 
 
-$(function(){
-    //cargar comunidades
-    $.getJSON( "https://rawgit.com/JorGS93/NoticiasWeb/master/js/news.json", function( jsonObject ) {
-        ponerTitulo( jsonObject );
-});
 
      function ponerTitulo(json){
   $.each( json, function( i, news ) {
